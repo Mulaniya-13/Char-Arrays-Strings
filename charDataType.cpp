cfb7f3323a396d8f8c592f0a6bcabc00357a554e
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-06-18 18:29:05 +0530

    First Commit

---
+++ charDataType.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // char ch1='A';
+    // char ch2='a';
+    // char ch3='0';
+    // char ch4='@';
+    // char ch5='\n'; // newline character
+
+    // char ch='A';
+    // char ch1='a';
+    // cout<<(int) ch<<endl; // ASCII value of 'A' is 65
+    // cout<<(int) ch1<<endl; // ASCII value of 'a' is 97
+
+    char ch='f';
+    int pos=ch-'a';
+    cout<<pos<<endl;
+    return 0;
+}
