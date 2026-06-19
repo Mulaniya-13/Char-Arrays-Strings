6563736a94201761685f2c1d4ed593eea081c15f
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-06-19 20:27:15 +0530

    updated File

--- characterArray.cpp
+++ characterArray.cpp
@@ -18,8 +18,21 @@ int main(){
     // char work[]="code";
     // char work[10]="code";
     // char work[]={'c','o','d','e','\0'};
-    char work[50]={'c','o','d','e','\0'};
-    cout<<work<<endl;
-    cout<< strlen(work)<<endl;
+    // char work[50]={'c','o','d','e','\0'};
+    // cout<<work<<endl;
+    // cout<< strlen(work)<<endl;
+
+    //input char array
+    // char word[30];
+    // cin>>word; //ignore whitespace
+
+    // cout<<"your word was : "<<word<<endl;
+    // cout<<"length : "<<strlen(word)<<endl;
+
+    char sentence[30];
+    cin.getline(sentence,30,'.'); //include whitespace
+
+    cout<<"your word was : "<<sentence<<endl;
+    cout<<"length : "<<strlen(sentence)<<endl;
     return 0;
 }
