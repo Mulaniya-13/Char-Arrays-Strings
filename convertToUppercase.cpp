bfd9a9d2a4092fd66e0f5165ebf23f08fa83a0c9
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-06-22 20:46:16 +0530

    Updating Files

--- convertToUppercase.cpp
+++ convertToUppercase.cpp
@@ -2,20 +2,34 @@
 #include<cstring>
 using namespace std;
 
-void toUpper(char word[],int n){
-      for(int i=0;i<n;i++){
+//uppercase
+// void toUpper(char word[],int n){
+//       for(int i=0;i<n;i++){
+//         char ch=word[i];
+//         if(ch>='A' && ch<='Z'){ //Upper Case
+//             continue;
+//         }else{ //lower case
+//             word[i]=ch-'a'+'A';
+//         }
+//       }
+// }
+
+//lowercase
+void toLower(char word[],int n){
+     for(int i=0;i<n;i++){
         char ch=word[i];
-        if(ch>='A' && ch<='Z'){ //Upper Case
+        if(ch>='a' && ch<='z'){
             continue;
-        }else{ //lower case
-            word[i]=ch-'a'+'A';
+        }else{
+            word[i]=ch-'A'+'a';
         }
-      }
+     }
 }
 
 int main(){
     char word[]="ApPle";
-    toUpper(word,strlen(word));
+    // toUpper(word,strlen(word));
+    toLower(word,strlen(word));
     cout<<word<<endl;
     return 0;
 }
