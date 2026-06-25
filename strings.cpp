f7831ef12400c84cc535dea38ba3948ae4b3de72
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-06-25 20:55:28 +0530

    Updating existing files

--- strings.cpp
+++ strings.cpp
@@ -3,12 +3,23 @@
 using namespace std;
 
 int main(){
-    string str;
-    getline(cin,str,'.');
-    cout<<str[0]<<endl;
-    cout<<str[1]<<endl;
-    cout<<str[2]<<endl;
+    // string str;
+    // getline(cin,str,'.');
+    // cout<<str[0]<<endl;
+    // cout<<str[1]<<endl;
+    // cout<<str[2]<<endl;
     // str="Hello, C++!";
     // cout<<str<<endl;
+
+    //for each loop
+    string str="Aashish Mulaniya";
+    // for(int i=0;i<str.length();i++){ //dot operator is used to access the length of the string
+    //     cout<<str[i]<<" ";
+    // }
+    // cout<<endl;
+
+    for(char ch:str){ //for each loop
+        cout<<ch<<" ";
+    }
     return 0;
 }
